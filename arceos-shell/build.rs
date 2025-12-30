27288ce41d666c9465943853c7bdf466a4e0b94c
张国宇 <zhangguoyu@zhangguoyudeMac-mini.local>
2025-12-30 18:13:07 +0800

    fix_mmu

--- arceos-shell/build.rs
+++ arceos-shell/build.rs
@@ -1,6 +1,6 @@
 // build.rs
 fn main() {
     println!("cargo:rustc-link-arg=-T");
-    println!("cargo:rustc-link-arg=rstiny/link.lds");
+    println!("cargo:rustc-link-arg=target/aarch64-unknown-none-softfloat/release/linker_axplat-aarch64-d3000m-n80-laptop.lds");
     println!("cargo:rustc-codegen-options=-C target-cpu=cortex-a76");
 }
